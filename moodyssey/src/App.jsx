0b70dcd1584df89645490d9b3a563dea14ce7113
rrejhine <rrejhine@gmail.com>
2024-01-14 13:26:19 +0800

    third commit
    instructions on main jsx file

--- moodyssey/src/App.jsx
+++ moodyssey/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1 className="text-3xl font-bold text-pink-700">
+        Moodyssey
       </h1>
     </>
   )
--- moodyssey/src/main.jsx
+++ moodyssey/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// You must import the screen/page you want to test first at this portion. Follow the line below.
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  {/* For testing a screen/page, change 'App' below into the name of the screen/page you're modifying. Note: You should import the screen/page file first. */}
     <App />
   </React.StrictMode>,
 )
+
+// To run, open a terminal and change directory to 'moodyssey'
+// After that, run this command 'npm run dev'
