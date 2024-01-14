d1a515ee05b60c2ffe9149601eda9744ece81aa7
rrejhine <rrejhine@gmail.com>
2024-01-14 14:03:35 +0800

    fourth commit
    Landing Page + Navbar Component

---
+++ moodyssey/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+
+    return (
+      <>
+        <nav>
+            <div className="bg-red-100 flex space-x-96">
+                <div className="nav-logo">                    
+                    <h1 className="text-3xl font-bold text-pink-700">
+                        Logo
+                    </h1>
+                </div>
+                <div className="nav-links">links</div>
+            </div>
+        </nav>
+      </>
+    )
+  }
+  
+  export default Navbar
+  
